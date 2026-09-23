168780ce596b22e883d67b907e1a6d142fc742d4
Lynda Gunnarsdottir <lg223fn@student.lnu.segit config --global user.emailgit config --global user.email lg223fn@student.lnu.segit config --global user.email.cler.clear$git config --global user.email lg223fn@student.lnu.se>
2026-09-24 00:16:16 +0200

    Initial project setup

---
+++ eslint.config.js
@@ -0,0 +1,4 @@
+import config from '@lnu/eslint-config'
+import prettier from 'eslint-config-prettier'
+
+export default [...config, prettier]
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+export default {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 120,
+  tabWidth: 2,
+  endOfLine: 'lf',
+}
